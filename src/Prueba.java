14e4232d2eda2ac528086e7d8d2fa7a24553cb11
Jhon Eliver Zapata <jezapata@softcaribbean.com>
2019-11-01 17:15:47 -0500

    Prueba de variable

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 	
 	private float primerAtributo2;
 	private double segundoAtributo;
-	private int tercerAtributo;
+	private int tercerAtributo3;
 	private int cuartoAtributo;
 	
 }
